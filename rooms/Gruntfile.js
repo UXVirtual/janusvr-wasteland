baf00da27619d00d45cd41eecc0f74a55aa399fc
Michael <michael@hazardmedia.co.nz>
2015-01-04 20:39:28 +1300

    Added support for AssetShader validation with external glsl-validator binary trigger by gruntfile. Added example shaders.

--- rooms/Gruntfile.js
+++ rooms/Gruntfile.js
@@ -65,14 +65,52 @@ module.exports = function(grunt) {
                 options: {
                     livereload: false
                 }
+            },
+            glsl:{
+                files: ['shaders/*.glsl'],
+                options: {
+                    livereload: false,
+                    nospawn: true
+                }
+            }
+        },
+        exec: {
+
+            glsl: {
+                command: function(filepath){
+                    return '../tools/glsl-validate '+filepath+' +profile=fragment';
+                },
+                callback: function(){
+                    //grunt.log.writeln('file is valid');
+                },
+                stdout: true,
+                stderr: true,
+                exitCode: 0
             }
         }
+    });
+
+    grunt.event.on('watch', function(action, filepath, target) {
+
+        //grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+        switch(target){
+            case 'glsl':
+                //watch for changes to glsl files and run external validation cli program to validate that glsl file compiles
+                if(action === 'added' || action === 'changed'){
+                    grunt.task.run('exec:glsl:'+filepath);
+                }
+
+                break;
+        }
+
+
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-exec');
 
     // Default task(s).
     grunt.registerTask('default', ['uglify']);
